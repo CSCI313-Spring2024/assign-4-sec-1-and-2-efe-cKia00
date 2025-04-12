2bfbbd114cbdff63dbe5517dcb4264c999894f7d
Efe Awo-Osagie <deafveid@gmail.com>
2025-04-11 19:44:51 -0500

    Finished the HTML and CSS design of app, edit-contact, and  add-contact components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { ListContactComponentComponent } from './list-contact-component/list-contact-component.component';
-import { AddContactComponentComponent } from './add-contact-component/add-contact-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ListContactComponentComponent, AddContactComponentComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
